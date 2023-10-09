773177d11986c8cbdf3410c909b3de36993640a3
dllamas01 <112565240+dllamas01@users.noreply.github.com>
2023-10-09 11:44:45 -0500

    hi
    jxwuiai

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-let products = [];
-router.get(  '/add-product', (req, res, next) => {
-    res.render( 'addProduct',
-        {
-            from: 'addProduct'
-        })
-});
-router.post(  '/product', (req, res, next) => {
-    // console.log( req );
-    console.log("----flag")
-    let t = req.body.title;
-    let a = req.body.author;
-    let p = req.body.price
-    products.push( {
-        title: t,
-        author: a,
-        price: p
-    })
-    res.redirect('/add-product')
-    // res.send(`Made it to post title:${t}`);
-});
+const productsController= require("../controller/products")
+// let products = [];
+// router.get(  '/add-product', (req, res, next) => {
+//     res.render( 'addProduct',
+//         {
+//             from: 'addProduct'
+//         })
+// });
+router.get('/add-product', productsController.getAddProduct)
+router.post('/product', productsController.postAddProduct)
+// router.post(  '/product', (req, res, next) => {
+//     // console.log( req );
+//     console.log("----flag")
+//     let t = req.body.title;
+//     let a = req.body.author;
+//     let p = req.body.price
+//     products.push( {
+//         title: t,
+//         author: a,
+//         price: p
+//     })
+//     res.redirect('/add-product')
+//   res.send(`Made it to post title:${t}`);
+// });
 
 // module.exports = router;
-exports.products = products;
+// exports.products = products;
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const adminData = require("./admin");
-router.get(  '/showProducts', (req, res, next) => {
-    console.log( "----------------");
-    console.log( adminData.products);
-    //res.sendFile( ( path.join(__dirname, '../views', 'showProducts.html')));
-    res.render( 'showProducts', {
-        title: "Show Available Products",
-        from: 'showProducts',
-        products : adminData.products
-    });
-});
+// const path = require('path');
+// const adminData = require("./admin");
+const productsController = require("../controller/products")
+
+router.get('/showProducts', productsController.getProducts)
+// router.get(  '/showProducts', (req, res, next) => {
+//     console.log( "----------------");
+//     console.log( adminData.products);
+//     //res.sendFile( ( path.join(__dirname, '../views', 'showProducts.html')));
+//     res.render( 'showProducts', {
+//         title: "Show Available Products",
+//         from: 'showProducts',
+//         products : adminData.products
+//     });
+// });
 module.exports = router;
